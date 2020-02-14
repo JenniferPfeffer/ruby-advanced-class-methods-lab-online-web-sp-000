b5e4678ab766d3cb265eb9bf7d7231b60b75d7b4
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-14 16:50:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   def self.find_or_create_by_name(name) 
     if @@all.include?(name) == false
       self.create_by_name(name)
-    else 
+    elsif @@all.include?(name) == true 
       self.find_by_name(name) 
     end 
   end
