a222a60024331c9d011c5b5c8080a6c313a27ae5
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-14 16:59:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,8 @@ class Song
   end
   def self.new_from_filename(name) 
     song = self.new 
-    song.name = name.split("-")[1].chomp()
+    song.name = name.split("-")[1].chomp(".mp3") 
+    song.artist = 
   def self.destroy_all 
     @@all.clear 
   end
